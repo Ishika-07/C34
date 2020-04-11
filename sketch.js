3bb6b68439d3993cf945a2c303b64d71dbe8c6e4
Ishika-07 <60807873+Ishika-07@users.noreply.github.com>
2020-04-11 15:57:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,63 @@
+var ball,ballImg;
+var database, position;
+var locnode;
+
+function preload(){
+
+    ballImg = loadImage("sprites/car.jpg");
+}
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database();
+    ball = createSprite(250,250,10,10);
+    ball.addImage ("img",ballImg);
+    ball.shapeColor = "red";
+    ball.scale =0.4;
+    locnode = database.ref("Ball/Position");
+    locnode.on("value",readPosition,showError);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+
+
+
+    drawSprites();
+}
+
+function writePosition(x,y){
+
+database.ref('Ball/Position').set(
+
+{
+    x:ball.x + x,
+    y : ball.y + y
+}
+)
+
+}
+function readPosition(data){
+
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+
+
+}
+function showError(){
+
+    console.log("Error");
+}
